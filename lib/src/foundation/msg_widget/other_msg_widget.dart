bf7826b63ea93c800104270904c852d45c28eeec
nirjalsebs <164319729+nirjalsebs@users.noreply.github.com>
2024-04-18 23:09:14 +0545

    changes

--- lib/src/foundation/msg_widget/other_msg_widget.dart
+++ lib/src/foundation/msg_widget/other_msg_widget.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_initicon/flutter_initicon.dart';
 
-class OtherMsgWidget extends StatelessWidget {
+class OtherMsgWidget extends StatefulWidget {
   final String sender;
   final String msg;
 
@@ -10,7 +9,18 @@ class OtherMsgWidget extends StatelessWidget {
       : super(key: key);
 
   @override
+  _OtherMsgWidgetState createState() => _OtherMsgWidgetState();
+}
+
+class _OtherMsgWidgetState extends State<OtherMsgWidget> {
+  String? _lastSender;
+
+  @override
   Widget build(BuildContext context) {
+    final isFirstMessageFromSender = widget.sender != _lastSender;
+
+    _lastSender = widget.sender;
+
     return Align(
       alignment: Alignment.bottomLeft,
       child: ConstrainedBox(
@@ -20,50 +30,59 @@ class OtherMsgWidget extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-              child: Text(
-                '$sender - 7:08 AM',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey[500],
-                ),
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 38, vertical: 0),
+            //   child: Text(
+            //     widget.sender,
+            //     style: TextStyle(
+            //       fontSize: 12,
+            //       color: Colors.grey[500],
+            //     ),
+            //     overflow: TextOverflow.ellipsis,
+            //   ),
+            // ),
             IntrinsicHeight(
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Initicon(
-                      text: 'Anna Shrestha',
-                      size: 32,
-                      elevation: 4,
-                      backgroundColor: Colors.purple,
-                    ),
-                  ),
-                  const SizedBox(
-                      width:
-                          2), // Adjust the spacing between initicon and message
-                  Expanded(
-                    child: Card(
-                      color: Colors.grey[200],
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10.0),
-                          topRight: Radius.circular(10.0),
-                          bottomRight: Radius.circular(10.0),
-                        ),
+                  if (isFirstMessageFromSender)
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Initicon(
+                        text: widget.sender,
+                        size: 32,
+                        elevation: 4,
+                        backgroundColor: Colors.purple,
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          msg,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.black,
+                    ),
+                  // if (isFirstMessageFromSender)
+                  const SizedBox(
+                    width: 2,
+                  ), // Adjust the spacing between initicon and message
+                  Padding(
+                    padding: isFirstMessageFromSender
+                        ? EdgeInsets.zero
+                        : const EdgeInsets.symmetric(
+                            horizontal: 32, vertical: 0),
+                    child: Flexible(
+                      child: Card(
+                        elevation: 0,
+                        color: Colors.grey[200],
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(8.0),
+                            topRight: Radius.circular(8.0),
+                            bottomRight: Radius.circular(8.0),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            widget.msg,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                            ),
                           ),
                         ),
                       ),
