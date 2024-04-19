c25c8c8be3437628d15f79c340714eed7ba89490
nirjalsebs <164319729+nirjalsebs@users.noreply.github.com>
2024-04-19 22:05:44 +0545

    emoji added

--- lib/src/pages/group/group_page.dart
+++ lib/src/pages/group/group_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/foundation/components/chat_welcome.dart';
 import 'package:flutter_application_1/src/foundation/components/date_display.dart';
@@ -6,6 +9,9 @@ import 'package:flutter_application_1/src/foundation/msg_widget/other_msg_widget
 import 'package:flutter_application_1/src/foundation/msg_widget/own_msg_widget.dart';
 import 'package:flutter_application_1/src/pages/group/msg_model.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:flutter/foundation.dart' as foundation;
 
 class GroupPage extends StatefulWidget {
   final String name;
@@ -54,6 +60,8 @@ class _GroupPageState extends State<GroupPage> {
     });
   }
 
+  bool _showEmoji = false;
+
   void sendMsg(String msg, String senderName) {
     MsgModel ownMsg = MsgModel(msg: msg, type: "ownMsg", sender: senderName);
     listMsg.add(ownMsg);
@@ -71,61 +79,107 @@ class _GroupPageState extends State<GroupPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Group Project One",
-            style: TextStyle(color: Colors.white),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Group Project One",
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.orange[400],
           ),
-          backgroundColor: Colors.orange[400],
-        ),
-        body: Column(
-          children: [
-            NewJoin(),
-            Welcome_Group_Chat(),
-            Date_Display(),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: listMsg.length,
-                    itemBuilder: (context, index) {
-                      if (listMsg[index].type == "ownMsg") {
-                        return OtherMsgWidget(
-                            msg: listMsg[index].msg,
-                            sender: listMsg[index].sender);
-                      } else {
-                        return OtherMsgWidget(
-                            msg: listMsg[index].msg,
-                            sender: listMsg[index].sender);
-                      }
-                    })),
-            Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                child: Row(
-                  children: [
-                    Expanded(
+          body: Column(
+            children: [
+              // NewJoin(),
+              // Welcome_Group_Chat(),
+              // Date_Display(),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: listMsg.length,
+                      itemBuilder: (context, index) {
+                        if (listMsg[index].type == "ownMsg") {
+                          return OtherMsgWidget(
+                              msg: listMsg[index].msg,
+                              sender: listMsg[index].sender);
+                        } else {
+                          return OtherMsgWidget(
+                              msg: listMsg[index].msg,
+                              sender: listMsg[index].sender);
+                        }
+                      })),
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                        height: 45,
                         child: TextFormField(
-                      controller: _msgController,
-                      decoration: InputDecoration(
-                          hintText: "Type here...",
-                          border: const OutlineInputBorder(
-                              borderSide: BorderSide(width: 2)),
-                          suffixIcon: IconButton(
-                              onPressed: () {
-                                String msg = _msgController.text;
-                                if (msg.isNotEmpty) {
-                                  sendMsg(_msgController.text, widget.name);
-                                }
-                                _msgController.clear();
-                              },
-                              icon: const Icon(
-                                Icons.send,
-                                color: Colors.orange,
-                              ))),
-                    )),
-                  ],
-                ))
-          ],
-        ));
+                          controller: _msgController,
+                          onTap: () => {
+                            setState(() {
+                              if (_showEmoji) {
+                                _showEmoji = !_showEmoji;
+                              }
+                            })
+                          },
+                          decoration: InputDecoration(
+                              hintText: "Type a message",
+                              hintStyle:
+                                  TextStyle(fontWeight: FontWeight.normal),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 12, horizontal: 10),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey.shade100, width: 2)),
+                              suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _showEmoji = !_showEmoji;
+                                    });
+                                  },
+                                  icon: const Icon(
+                                    Icons.emoji_emotions_outlined,
+                                    // color: Colors.orange,
+                                  ))),
+                        ),
+                      )),
+                      IconButton(
+                          onPressed: () {
+                            String msg = _msgController.text;
+                            if (msg.isNotEmpty) {
+                              sendMsg(_msgController.text, widget.name);
+                            }
+                            _msgController.clear();
+                          },
+                          icon: const Icon(
+                            Icons.send,
+                            color: Colors.orange,
+                          )),
+                    ],
+                  )),
+              if (_showEmoji)
+                SingleChildScrollView(
+                  child: SizedBox(
+                    // height: 20,
+                    child: EmojiPicker(
+                        textEditingController: _msgController,
+                        config: Config(
+                          emojiViewConfig: EmojiViewConfig(
+                            emojiSizeMax: 28 *
+                                (foundation.defaultTargetPlatform ==
+                                        TargetPlatform.iOS
+                                    ? 1.20
+                                    : 1.0),
+                          ),
+                          height: 256,
+                        )),
+                  ),
+                )
+            ],
+          )),
+    );
   }
 }
